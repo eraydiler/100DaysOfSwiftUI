42899e6359b47d935a613bbb1251a43805afd8ad
eraydiler <eray.diler@gmail.com>
2023-09-05 18:09:02 +0300

    Rearranged Workspace's directories.

---
+++ 3-GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Eray Diler on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
