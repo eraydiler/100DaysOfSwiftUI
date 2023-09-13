df4c8d7cae74d7fe4b5918339f792e8b4a060e54
eraydiler <eray.diler@gmail.com>
2023-09-13 19:54:53 +0300

    Added new project BetterRest to workspace.

---
+++ 6-BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Eray Diler on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 6-BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Eray Diler on 13.09.2023.
+//
+
+/*
+  Day26-28: Project 4
+  https://www.hackingwithswift.com/100/swiftui/28
+*/
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
