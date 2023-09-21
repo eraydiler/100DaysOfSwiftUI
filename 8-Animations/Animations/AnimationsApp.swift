12c7878cf7626f728c89ed2593313c8bd1418134
eraydiler <eray.diler@gmail.com>
2023-09-21 14:29:31 +0300

    Added new project `Animations` to the workspace. Renamed WordScramble project's directory on locally.

---
+++ 8-Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Eray Diler on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
