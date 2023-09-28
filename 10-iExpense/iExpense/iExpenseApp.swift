3f8fa4343e1dd27a9f44192fe75bafc4bc8ff93c
eraydiler <eray.diler@gmail.com>
2023-09-28 21:33:58 +0300

    Added new project iExpense into workspace.

---
+++ 10-iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Eray Diler on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
