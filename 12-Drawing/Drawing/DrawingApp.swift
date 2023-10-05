6289d56065f1182dc7f759364743c9bb6dd7e529
eraydiler <eray.diler@gmail.com>
2023-10-05 20:26:04 +0300

    Added new project Drawing into the workspace.

---
+++ 12-Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Eray Diler on 5.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
