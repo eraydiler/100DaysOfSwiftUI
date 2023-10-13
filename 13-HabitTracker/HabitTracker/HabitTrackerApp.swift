57973ed4b9e9f1bdab19eeae6cacd916d66d7c3e
eraydiler <eray.diler@gmail.com>
2023-10-13 17:59:50 +0300

    Added new project HabitTracker into Workspace.

---
+++ 13-HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Eray Diler on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
