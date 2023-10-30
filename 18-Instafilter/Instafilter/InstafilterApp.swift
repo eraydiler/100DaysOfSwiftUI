54c79c45fcffa7c1c3f4796a505059eb928fc7b1
eraydiler <eray.diler@gmail.com>
2023-10-30 21:10:18 +0300

    Added new project Instafilter into workspace.

---
+++ 18-Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Eray Diler on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
