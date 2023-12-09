83afda1e580bc93563e4bfce31c69a9ff3c79801
eraydiler <eray.diler@gmail.com>
2023-12-09 15:58:00 +0300

    Added new project HotProspects into the workspace.

---
+++ 22-HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Eray Diler on 9.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
