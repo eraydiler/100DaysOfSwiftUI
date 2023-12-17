84cbf106e61713bba9bee639b0678c2d73efced6
eraydiler <eray.diler@gmail.com>
2023-12-17 18:57:07 +0300

    Added new project Filezilla into the workingspace.

---
+++ 23-Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Eray Diler on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
